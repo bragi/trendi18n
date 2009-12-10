da048e04f47d75f5fb184ee3cb41fe3cbd6e6521
root <root@piotrek-desktop.(none)>
2009-12-10 10:26:26 +0100

    Solved problem with migrations (https://github.com/bragi/trendi18n/issues#issue/17)

--- spec/test_application/db/migrate/20091208195455_create_translations.rb
+++ spec/test_application/db/migrate/20091208195455_create_translations.rb
@@ -1,18 +1,19 @@
 class CreateTranslations < ActiveRecord::Migration
   def self.up
     create_table ActiveRecord::Base.pluralize_table_names ? :translations : :translation do |t|
-      t.string :locale
-      t.string :key
+      t.string :locale, :null => false, :limit => 6
+      t.string :key, :null => false
       t.string :scope
-      t.text :default
+      t.text :default, :null => false
       t.text :translation
       t.text :zero
       t.text :one
       t.text :many
       t.text :few
-      t.string :status
+      t.string :state, :null => false
       t.timestamps
     end
+    add_index :translations, [:locale, :key, :scope], :unique => true
   end
 
   def self.down
